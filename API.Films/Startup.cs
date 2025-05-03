29b2d02cec77d363087882c1e33920accfd75a24
Pablo Nuñez Ramon <pnr91@proton.me>
2025-05-03 18:11:38 +0200

    .

---
+++ API.Films/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Films;
+
+namespace API.Films
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+                );
+
+            //services.AddScoped<IGeneroRepository, GeneroRepository>();
+            //services.AddScoped<IGeneroService, GeneroService>();
+
+
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ Persistence.Films/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Films.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Films
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected AppDbContext()
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
