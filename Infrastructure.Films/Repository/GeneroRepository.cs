aaa8a8d39d11265115aebb566e35d6e946bc1eb5
Pablo Nuñez Ramon <pnr91@proton.me>
2025-05-03 18:19:07 +0200

    Repository Genero

---
+++ Infrastructure.Films/Repository/GeneroRepository.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Films.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Films;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Films.Repository
+{
+    public class GeneroRepository : IGeneroRepository
+    {
+        private readonly AppDbContext _context;
+
+        public GeneroRepository(AppDbContext applicationDbContext)
+        {
+            this._context = applicationDbContext;
+        }
+
+        public async Task<IEnumerable<Genero>> GetAllAsync()
+        {
+            var result = await _context.Generos.ToListAsync();
+
+            return result;
+        }
+    }
+}
