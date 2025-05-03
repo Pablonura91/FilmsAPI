753f0108ab4c557d90e01e64cc37a97b9a567a10
Pablo Nuñez Ramon <pnr91@proton.me>
2025-05-03 18:16:02 +0200

    Automapper y DTOs

---
+++ Z.Extra.Mapper/GeneroProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Domain.Films.Entities;
+using DTOs.Genero;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z.Extra.Mapper
+{
+    public class GeneroProfile : Profile
+    {
+        public GeneroProfile()
+        {
+            CreateMap<Genero, GeneroGetAllDTO>().ReverseMap();
+        }
+    }
+}
